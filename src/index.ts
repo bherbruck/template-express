b550c87a05881119696f1b7c99db711def106724
bherbruck <brennen.herbruck@gmail.com>
2021-05-10 10:48:40 -0400

    add helmet and cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express'
 import config from './config'
+import cors from 'cors'
+import helmet from 'helmet'
 
 const PORT = config.port
 
 const app = express()
+
+app.use(cors())
+app.use(helmet())
 
 app.get('/', async (req, res) => {
   res.send({ message: 'Hello, world!' })
